5f90657505db7f340064edb47f62c569b2d7a4df
Юлия <160242381+Julia-Achoo@users.noreply.github.com>
2024-03-23 20:48:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,111 +1,50 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-    return 0;
-#include <alg.h>
-#include <iostream>
-#include <cstdint>
-int countPairs1(int* arr, int len, int value) {
     int count = 0;
-    for (int i = 0; i < (len - 1); i++) {
+    for (int i = 0; i < len; i++) {
         for (int j = i + 1; j < len; j++) {
-            if ((arr[i] + arr[j]) == value) {
+            if (arr[i] + arr[j] == value) {
                 count++;
-            }
-        }
-    }
-    if (count > (0)) {
-        return count;
-    }
-    return 0;
-}
-int countPairs2(int *arr, int len, int value) {
-    return 0;
-int countPairs2(int* arr, int len, int value) {
-    int count = 0, m = 0, p = 0;
-    for (int i = 0; i < (len - 1); i++) {
-        if ((arri + arri + 1) >= value) {
-            m = i;
-            break;
-        }
-    }
-    for (int i = 0; i < (len - 1); i++) {
-        if (arri > value) {
-            break;
-        }
-        if (m <= i) {
-            p = i + 1;
-        } else {
-            p = m;
-        }
-        for (; p < len; p++) {
-            if ((arri + arrp) == value) {
-                count++;
-            }
-            if (arrp > value) {
-                break;
             }
         }
     }
     return count;
 }
+
+int countPairs2(int *arr, int len, int value) {
+    int count = 0;
+    int left = 0;
+    int right = len - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == value) {
+            count++;
+            left++;
+            right--;
+        } else if (sum < value) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return count;
+}
+
 int countPairs3(int *arr, int len, int value) {
-    return 0;
-int countPairs3(int* arr, int len, int value) {
-    int count = 0, p = 0, min = 0, max = len;
-    for (int i = 0; i < (len - 1); i++) {
-        if (arr[i] > value) {
-            break;
-        }
-        min = i;
-        max = len - 1;
-        while (min < max) {
-            p = min + ((max - min) / 2);
-            if (min == max) {
-                break;
-            }
-            if ((arr[i] + arr[p]) > value) {
-                for (int n = (p - 1); n >= min; n--) {
-                    max = n + 1;
-                    if (arr[p] != arr[n]) {
-                        break;
-                    }
-                }
-                if (max - min == 1) {
-                    break;
-                }
-            }
-            if ((arr[i] + arr[p]) < value) {
-                for (int n = (p + 1); n <= max; n++) {
-                    min = n - 1;
-                    if (arr[p] != arr[n]) {
-                        break;
-                    }
-                }
-                if (max - min == 1) {
-                    break;
-                }
-            }
-            if ((arr[i] + arr[p]) == value) {
-                int r = max;
-                for (int n = (p + 1); n <= r; n++) {
-                    max = n - 1;
-                    if (arr[p] != arr[n]) {
-                        break;
-                    }
-                }
-                r = min;
-                for (int n = (p - 1); n >= r; n--) {
-                    min = n + 1;
-                    if (arr[p] != arr[n]) {
-                        break;
-                    }
-                }
-                break;
-            }
-        }
-        for (int j = min; j <= max; j++) {
-            if (((arr[i] + arr[j]) == value) && (i != j)) {
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        int target = value - arr[i];
+        int left = i + 1;
+        int right = len - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
                 count++;
+                break;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
     }
